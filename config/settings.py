2f694d08cffdccc977b69f4688895117daa17204
Tomide <tomideerinola@gmail.com>
2026-02-28 17:32:47 -0800

    worked on configurations for deployment and fixed every issue noticed after deployment

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5e0%ryb@2e8%=rr!cf+n8s#4pjhfm%v2xny!=y2^y!-psr!on_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pointa.pythonanywhere.com']
 
 
 # Application definition
@@ -129,3 +129,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+STATIC_ROOT= BASE_DIR / "staticfiles"
